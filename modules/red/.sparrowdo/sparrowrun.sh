06f3bf7e8271bd85e18172c3a756e0d61095b7e1
Alexey Melezhik <melezhik@gmail.com>
2020-01-01 16:36:33 -0600

    remove cache files

--- modules/red/.sparrowdo/sparrowrun.sh
+++
@@ -1,7 +0,0 @@
-set -e
-cd /root/.sparrowdo
-export PATH=/opt/rakudo-pkg/bin/:$PATH
-export SP6_CONFIG=config.pl6
-export SP6_REPO=http://repo.westus.cloudapp.azure.com
-perl6 -MSparrow6::Task::Repository -e Sparrow6::Task::Repository::Api.new.index-update
-perl6 -MSparrow6::DSL sparrowfile
