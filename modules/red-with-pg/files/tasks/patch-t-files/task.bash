f87c22de9762083423fea318881aa82d9133bf11
Alexey Melezhik <melezhik@gmail.com>
2020-01-13 01:01:31 +0000

    using RED_DATABASE

--- modules/red-with-pg/files/tasks/patch-t-files/task.bash
+++ modules/red-with-pg/files/tasks/patch-t-files/task.bash
@@ -4,7 +4,7 @@ dir=$(config dir)
 
 cd $dir
 
-perl -i -p -e 'my $id = 1; s/database "SQLite".*$/q{database "Pg", :dbname<red} . $i++ . q{>;}/ge' t/*.t
+perl -i -p -e 'my $id = 1; s/%\*ENV<RED_DATABASE>/q|"host=localhost dbname=red| . $i++ . q|"|/ge' t/*.t
 
-grep  'database "Pg"' t/*.t
+grep  'host=localhost dbname=red' t/*.t
 
