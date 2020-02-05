d9c0d3b0951c655e7851ee1483abd7a5d07790dd
Alexey Melezhik <melezhik@gmail.com>
2020-02-05 19:12:04 +0000

    copy RakuDistApi files

---
+++ app.pl
@@ -0,0 +1,146 @@
+#!/usr/bin/perl
+
+use Mojolicious::Lite;
+
+get '/rakudist/api/status' => sub {
+
+  my $c = shift;
+
+  my $out = `docker ps`;
+
+  $out .= "\n=========================================\n";
+
+  $out .= `ps uax| grep 'sparrowdo --no'|grep -v grep`;
+
+  $out .= "\n\nalpine-rakudist\n";
+
+  $out .= `docker exec -i alpine-rakudist ps uax`;
+
+  $out .= "\ndebian-rakudist\n";
+
+  $out .= `docker exec -i debian-rakudist ps uax`;
+
+  $out .= "\ncentos-rakudist\n";
+
+  $out .= `docker exec -i centos-rakudist ps uax`;
+
+  
+  $c->render(text => "<pre>$out</pre>");
+
+};
+
+get '/rakudist' => sub {
+
+  my $c = shift;
+
+  return $c->render(text => "Welcome to RakuDist - easy way to test your Raku modules distributions across different OS");
+
+};
+
+
+post '/rakudist/api/run/:thing' => sub {
+
+  my $c = shift;
+
+  my $thing = $c->stash('thing');
+  my $os = $c->param('os');
+  my $project = $c->param('project');
+  my $rakudo_version = $c->param('rakudo_version') || "default";
+  my $sync_mode = $c->param('sync_mode') || "off";
+  my $type = ($thing eq ":github") ? "github" : "basic";
+  my $verbose = $c->param('verbose') || 0;
+
+  my $thing_to_run;
+
+  if ($type eq 'github' ){
+
+    unless ($project=~/^\S+\/\S+$/) {
+      return $c->render(text => "bad project param", status => 400)
+    }
+
+    $thing_to_run = $project
+
+  } else {
+
+    $thing_to_run = $thing
+    
+  }
+
+  unless ($os) {
+    return $c->render(text => "os param is required", status => 400)
+  }
+
+  unless ($thing=~/^([\w+\-\d:])+$/) {
+    return $c->render(text => "bad project param", status => 400)
+  }
+
+  unless ($os=~/^(alpine|debian|centos)+$/) {
+    return $c->render(text => "bad os param", status => 400)
+  }
+
+  my $id = time();
+
+  my $out = `/usr/bin/rkd-run $thing_to_run $os $type $rakudo_version $sync_mode $id`;
+
+  my $exit_code = $?;
+
+  if ($sync_mode eq "on") {
+    $c->res->headers->header('X-RakuDist-ExitCode' => $exit_code );
+    $c->render(text => $out);
+  } else {
+    if ($exit_code == 0){
+      $c->render(text => $out);
+    } else {
+      $c->render(text => $out, status => 500)
+    }
+  }
+};
+
+
+post '/rakudist/api/job/status' => sub {
+
+  my $c = shift;
+
+  my $token = $c->param('token');
+
+  my ($id, $docker_id) = split /:/, $token;
+
+  `ps ax | grep sparrowdo | grep -q "\\--prefix=$id \\--"`;
+  
+  my $exit_code = $?;
+
+  if ($exit_code == 0){
+    $c->render(text => "running");
+  } else {
+      if ( open(my $fh, "<", "/usr/share/repo/rakudist/reports/$docker_id/$id.txt")) {
+        my $report = join "", <$fh>;
+        close $fh;
+        if ($report =~ /RakuDist: OK/){
+         $c->render(text => "success" );
+       } else {
+          $c->render(text => "fail" );
+        }
+      } else {
+        $c->render(text => "unknown");
+      }
+  }
+
+
+};
+
+post '/rakudist/api/job/report' => sub {
+
+  my $c = shift;
+
+  my $token = $c->param('token');
+
+  my ($id, $docker_id) = split /:/, $token;
+
+  $c->redirect_to("/rakudist/reports/$docker_id/$id.txt");
+
+};
+
+app->start;
+
+
+ 
---
+++ bin/docker-rotate.sh
@@ -0,0 +1,19 @@
+set -x
+docker ps
+docker stop -t 1 debian-rakudist
+docker stop -t 1 alpine-rakudist
+docker stop -t 1 centos-rakudist
+docker run -d -t --rm --name debian-rakudist 1b41711bb6c8
+docker run -d -t --rm --name alpine-rakudist c70676a7b62b
+docker run -d -t --rm --name centos-rakudist 0bfb68601d70
+docker ps
+
+cd ~/projects/RakuDist/dockers/debian
+sparrowdo --bootstrap --docker=debian-rakudist --no_sudo --repo=http://repo.westus.cloudapp.azure.com
+
+cd ~/projects/RakuDist/dockers/centos
+sparrowdo --bootstrap --docker=centos-rakudist --no_sudo --repo=http://repo.westus.cloudapp.azure.com
+
+cd ~/projects/RakuDist/dockers/alpine
+sparrowdo --bootstrap --docker=alpine-rakudist --no_sudo --repo=http://repo.westus.cloudapp.azure.com
+
