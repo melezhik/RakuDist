b6291acc2ad19e8b5d47d29b2cd90bd39182608c
Alexey Melezhik <melezhik@gmail.com>
2020-05-04 14:44:16 +0000

    improve my utilites

--- utils/queue-modules-with-native-deps.sh
+++ utils/queue-modules-with-native-deps.sh
@@ -1,1 +1,1 @@
-perl -n -e '@f = split; print "curl -d os=debian -d sparky=1 http://repo.westus.cloudapp.azure.com/rakudist/api/run/$f[0]\n"' utils/modules.list | sh
+perl -n -e '@f = split; print "echo ; curl -n $f[0]; curl -d sparky=1 http://repo.westus.cloudapp.azure.com/rakudist/api/run/$f[0]\n"' utils/modules.list | sh
--- utils/queue-selected-modules.sh
+++ utils/queue-selected-modules.sh
@@ -1,2 +1,2 @@
 export shai=$1
-perl -n -e '@f = split; print "curl -d os=debian -d rakudo_version=$ENV{shai} http://repo.westus.cloudapp.azure.com/rakudist/api/run/$f[0]; echo\n"' utils/selected.list | sh
+perl -n -e '@f = split; print "echo -n $f[0] ; curl -s -d os=debian -d rakudo_version=$ENV{shai} http://repo.westus.cloudapp.azure.com/rakudist/api/run/$f[0]; echo\n"' utils/selected.list | sh
