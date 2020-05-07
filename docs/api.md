131ed1aef348d87463d86fda3a964002054ed17b
Alexey Melezhik <melezhik@gmail.com>
2020-05-07 20:48:21 +0000

    clean things up

--- docs/api.md
+++ docs/api.md
@@ -71,6 +71,22 @@ Status:
 * -1  `fail` - a build failed
 * ""  (empty value) `unknown` - could not get a build status
 
+# Bash automation example
+
+```bash
+token=$(curl -s -d thing=Kind http://repo.westus.cloudapp.azure.com/rakudist2/queue)
+echo $token
+while true; do
+  status=$(curl -s http://repo.westus.cloudapp.azure.com/sparky/status/$token)
+  sleep 5
+  echo $status
+  if test -z "$status" || test "$status" -eq "1" || test "$status" -eq "-1"; then
+    break
+  fi
+done
+echo "result: $status"
+```
+
 # Travis integration example
 
 To test a GitHub project named `$project` on certain `$os`, just use RakuDist helper:
--- run-test.sh
+++ run-test.sh
@@ -1,13 +1,14 @@
-token=$(curl -s -d os=debian http://repo.westus.cloudapp.azure.com/rakudist/api/run/Kind)
+token=$(curl -s -d thing=Kind http://repo.westus.cloudapp.azure.com/rakudist2/queue)
 echo $token
 while true; do
-  status=$(curl -s -d token=$token http://repo.westus.cloudapp.azure.com/rakudist/api/job/status)
+  status=$(curl -s http://repo.westus.cloudapp.azure.com/sparky/status/$token)
   sleep 5
   echo $status
-  if [ $status != "running" ]; then
+  if test -z "$status" || test "$status" -eq "1" || test "$status" -eq "-1"; then
     break
   fi
 done
-echo "test: $status"
-curl -L -s -d token=$token http://repo.westus.cloudapp.azure.com/rakudist/api/job/report
+echo "result: $status"
+
+#curl -L -s -d token=$token http://repo.westus.cloudapp.azure.com/rakudist/api/job/report
 
