800a309d1f6c193943044c99532159a742689067
Alexey Melezhik <melezhik@gmail.com>
2020-06-21 17:24:38 -0500

    Update queue-selected-modules.sh

--- utils/queue-selected-modules.sh
+++ utils/queue-selected-modules.sh
@@ -1,2 +1,2 @@
-export shai=$1
-perl -n -e '@f = split; print "echo -n $f[0] ; curl -s -d os=debian -d rakudo_version=$ENV{shai} -d thing=$f[0] http://rakudist.raku.org/queue; echo\n"' utils/selected.list | sh
+export v=$1
+perl -n -e '@f = split; print "echo -n $f[0] ; curl -s -d os=debian -d rakudo_version=$ENV{v} -d thing=$f[0] http://rakudist.raku.org/queue; echo\n"' utils/selected.list | sh
